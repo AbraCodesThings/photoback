074daf736198f31787e49cd62b53d51856b1a7e5
Abraham Díaz <abradiazmediavilla@gmail.com>
2022-06-05 21:28:13 +0200

    Commit inicial

---
+++ resources/views/placeholder.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Uh Oh.</title>
+  </head>
+  <body>
+    <p>
+      <h1>ESTO ES UNA API.</h1>
+      <span>LÁRGATE.</span>
+    </p>
+  </body>
+</html>
