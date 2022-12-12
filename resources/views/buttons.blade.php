2cb42db9720f624a3f699e5aea7664083f74cb44
Abraham Díaz <abradiazmediavilla@gmail.com>
2022-12-12 19:00:54 +0100

    Intento de hacer el panel de navegación responsive; aún tiene fallos

--- resources/views/buttons.blade.php
+++ resources/views/buttons.blade.php
@@ -1,7 +1,7 @@
 
-<div id="buttons" class="d-flex p-3 my-2 justify-content-center mx-5 flex-row card card-body border rounded shadow">
+<div id="nav-buttons" class="d-flex flex-row justify-content-center p-3 my-2 mx-5 card card-body border rounded shadow">
     @if(Auth::guest())
-    <a href="{{route('signin')}}" class="btn btn-primary shadow">Create account</a>
+    <a href="{{route('signin')}}" class="btn btn-primary mx-4 shadow">Create account</a>
     <a href="{{route('login')}}" class="btn btn-primary mx-4 shadow">Login</a>
     @elseif(Auth::user())
     <a href="{{route('upload-form')}}" class="btn btn-primary mx-4 shadow">Upload image</a>
@@ -9,7 +9,20 @@
     <a href="{{route('logout')}}" class="btn btn-primary mx-4 shadow">Logout</a>
     <a href="{{route('gallery', ['user' => Auth::user()->name])}}" class="btn btn-primary mx-4 shadow">My gallery</a>
 
-    
     @endif
-    <a href="{{route('devinfo')}}" class="btn btn-primary shadow">Info for developers</a>
+    <a href="{{route('devinfo')}}" class="btn btn-primary mx-4 shadow">Info for developers</a>
 </div>
+
+<style>
+         @media(max-width: 600px){
+            #nav-buttons{
+                flex-direction: column !important;
+            }
+            #nav-buttons a{
+                margin-top: 2%;
+                margin-bottom: 2%;
+            }
+         }
+</style>
+    
+
