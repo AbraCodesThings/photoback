7c441f8f6610e046e542cf8e2d8f63952dba1abb
Abraham Díaz <abradiazmediavilla@gmail.com>
2022-12-13 12:03:07 +0100

    sike :) guarda los archivos peazo loko

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -19,23 +19,52 @@ class LoginController extends Controller
         ]);
     }
 
+    // public function authenticate(Request $request){
+
+    //     $request->validate([
+    //         'name' => 'required | max:25',
+    //         'password' => 'required | max:16'
+    //     ]);
+        
+    //     if(Auth::attempt(['name' => $request['name'], 'password' => $request['password']])){
+    //         return redirect()->intended('home')->withSuccess('Logged in!');
+    //     } else {
+    //        // dd($request);  
+    //         return redirect()->back()->withErrors('Something failed :(');
+    //     }
+    // }
+
     public function authenticate(Request $request){
 
         $request->validate([
             'name' => 'required | max:25',
             'password' => 'required | max:16'
         ]);
-        
-        if(Auth::attempt(['name' => $request['name'], 'password' => $request['password']])){
+
+        //TODO: quizá hacer una condición de que valide o no
+
+        if($this->checkUserExists($request) == false)
+            return redirect()->back()->withErrors('That user does not exist!');
+        if($this->checkPassword($request)){
             return redirect()->intended('home')->withSuccess('Logged in!');
         } else {
-           // dd($request);  
-            return redirect()->back()->withErrors('Something failed :(');
+            return redirect()->back()->withErrors('Incorrect password! Try again?');
         }
     }
     
     public function logout(){
         Auth::logout();
         return redirect()->back();
+    }
+
+    private function checkUserExists(Request $request){
+        $user = User::where('name', $request['name'])->get()->first();
+        if($user) return true;
+        else return false;
+    }
+
+    private function checkPassword(Request $request){
+        $success = Auth::attempt(['name' => $request['name'], 'password' => $request['password']]);
+        return $success;
     }
 }
